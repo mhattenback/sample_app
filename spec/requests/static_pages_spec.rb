1d96177c9482b1c01ec82da7529be1af92512641
Max Hattenback <max.hattenback@gmail.com>
2013-01-28 23:59:45 -0500

    Finish contact page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,10 +1,8 @@
 require 'spec_helper'
 
 describe "StaticPages" do
-
-
+################################################################################################
   describe "Home Page" do
-  
     it "should have the h1 'Sample App'" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
       visit '/static_pages/home'
@@ -15,7 +13,7 @@ describe "StaticPages" do
     	page.should have_selector('title', :text => 'Ruby on Rails Tutorial Sample App | Home')
 		end
 	end
-	
+################################################################################################	
 	describe "Help page" do
 		it "should have the h1 'Help'" do
 			visit '/static_pages/help'
@@ -26,20 +24,25 @@ describe "StaticPages" do
     	page.should have_selector('title', :text => 'Ruby on Rails Tutorial Sample App | Help')
 		end
   end
-  
-  
-  describe "About page" do
-  
+######################################################################################################
+  describe "About page" do																																						  
   	it "should have the h1 'About Us'" do
   		visit '/static_pages/about'
   		page.should have_selector('h1', :text => 'About Us')
   	end
-  	
   	it "should have the title 'About Us'" do
     	visit '/static_pages/about'
     	page.should have_selector('title', :text => 'Ruby on Rails Tutorial Sample App | About Us')
-		end
-		
-  end
+		end		
+	 end
+####################################################################################################### 
+	describe "Contact Page" do
+		it "should have the title 'Contact'" do
+			visit '/static_pages/contact'
+			page.should have_selector('title', :text => 'Ruby on Rails Tutorial Sample App | Contact' )
+			end
+		end	
+##########################################################################################################
 end
+
 
