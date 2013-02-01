ef534f3e5d82d4d2489f43a073e149a5b2a32ead
Max Hattenback <max.hattenback@gmail.com>
2013-02-01 06:42:39 -0500

    tsT

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,6 +8,7 @@
 #  created_at      :datetime        not null
 #  updated_at      :datetime        not null
 #  password_digest :string(255)
+#  remember_token  :string(255)
 #
 
 require 'spec_helper'
@@ -23,6 +24,7 @@ describe User do
 	it { should respond_to(:password_digest) }
 	it { should respond_to(:password) }
 	it { should respond_to(:password_confirmation) }
+	it { should respond_to(:remember_token) }
 	it { should respond_to(:authenticate) }
 	
 	it { should be_valid }
@@ -60,7 +62,6 @@ describe User do
 			end
 		end
 	end
-	#notes notse noets
 	describe "when email address is already taken" do
 		before do
 			user_with_same_email = @user.dup
@@ -104,8 +105,10 @@ describe User do
 			@user.email = mixed_case_email
 			@user.save
 			@user.reload.email.should == mixed_case_email.downcase
-			
-		
 		end
+	end
+	describe "remember token" do
+		before { @user.save }
+		its(:remember_token) { should_not be_blank }
 	end
 end
